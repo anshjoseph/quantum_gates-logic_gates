d1c5c417d4fb056a3bfab998d54fb06f90821737
R ansh joseph <70263460+anshjoseph@users.noreply.github.com>
2021-05-01 12:01:03 +0530

    Update gates.py

--- module_gates/gates.py
+++ module_gates/gates.py
@@ -5,6 +5,20 @@ Des:
     This is a 2bit adder
 
 """
+def And(curcuit,applied_reg,data_reg):
+    if len(applied_reg) == 2 and len(data_reg) == 1:
+        curcuit.cxx(applied_reg[0],applied_reg[1],data_reg[0])
+        return True
+    else:
+        return False
+def Or(curcuit,applied_reg,data_reg):
+    if len(applied_reg) == 2 and len(data_reg) == 1:
+        curcuit.cx(applied_reg[0],data_reg[0])
+        curcuit.cx(applied_reg[1],data_reg[0])
+        curcuit.cxx(applied_reg[0],applied_reg[1],data_reg[0])
+        return True
+    else:
+        return False
 def xor(curcuit,applied_reg,process_reg,data_reg):
     if len(applied_reg) == 2 and len(process_reg) == 3:
         curcuit.x(applied_reg[0])
